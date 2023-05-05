6930c00942043ed60e40bda27b409f783ac9c662
ericz02 <ezheng0720@gmail.com>
2023-05-05 01:30:18 -0400

    working on nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+
+          {/* Sidebar */}
+          {/* ClientProvider - Notification */}
+          
+
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      ChatGPT Messenger
+    <div className="text-white flex flex-col items-center justify-center h-screen px-2 ">
+      <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+ 
+      <div></div>
     </div>
   )
 }
